00e9dbc613f2c311b73c5f13cff026bcf1b7766d
qoqomi <energyy044@gmail.com>
2022-08-30 23:54:00 +0900

    basic

--- 1-types/1-1-basic.ts
+++ 1-types/1-1-basic.ts
@@ -1,6 +1,27 @@
 { 
-    //JavaScript 
-    //let
-    //const 
-    
+    //Primitive: number, symbol, null, undefined, boolean, string, bigint
+    // Object : function ,, 
+
+    //number 
+    const num: number = 5;
+   //boolean
+    const boal: boolean = false;
+    //string 
+    let age: boolean | string;
+    age = "hi";
+    age = false;
+  
+    function find() : boolean|undefined{
+        return false
+    }
+    //null
+    let person: null; //💩
+    let person2: string | null;
+
+
+    //unknown : 알 수 없는  어떤 데이터든 담을 수 있다. 
+    let notSure: unknown = 0;
+    //any
+    let anything :any 
+
 }
--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -6,6 +6,7 @@
     const BEANS_GRAMM_PER_SHOT: number = 7;
     let coffeeBeans: number = 0;
 
+    
     function makeCoffee(shots:number) : CoffeeCup{
         if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
             throw new Error("에러임")
