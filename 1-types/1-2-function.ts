0937c8f6d00436c498b376fa3cc706178c283918
qoqomi <energyy044@gmail.com>
2022-09-02 01:20:52 +0900

    function parameter

--- 1-types/1-2-function.ts
+++ 1-types/1-2-function.ts
@@ -1,28 +1,50 @@
 {
     //javaSctipt 💩
-    function jsAdd(num1, num2) {
-        return num1 + num2;
-    }
-    //TypeScript
-    function add(num1:number, num2:number):number {
-        return num1 + num2;
-    }
+    // function jsAdd(num1, num2) {
+    //     return num1 + num2;
+    // }
+    // //TypeScript
+    // function add(num1:number, num2:number):number {
+    //     return num1 + num2;
+    // }
     
-    function jsFetchNum(id) {
-        //code...
-        //code..
-        //code..
-        return new Promise((resolve,reject) => {
-            resolve(100)
-        })
+    // function jsFetchNum(id) {
+    //     //code...
+    //     //code..
+    //     //code..
+    //     return new Promise((resolve,reject) => {
+    //         resolve(100)
+    //     })
+    // }
+    // //TypeScript->문서화효과를 볼 수 있음.
+    // function jsTypeFetchNum(id:string):Promise<number> {
+    //     //code...
+    //     //code..
+    //     //code..
+    //     return new Promise((resolve,reject) => {
+    //         resolve(100)
+    //     })
+    // }
+
+    //JavaScript =>TypeScript
+    //Optional parameter -> 전달받을 수도 있고 전달받지 않을 수도 있다! 
+    function printName(firstName: string, lastName?: string) {
+        console.log(firstName, lastName)
     }
-    //TypeScript
-    function jsTypeFetchNum(id:string):Promise<number> {
-        //code...
-        //code..
-        //code..
-        return new Promise((resolve,reject) => {
-            resolve(100)
-        })
+    printName('Hi', 'name')
+    printName('firstName')
+
+    //Default parameter:기본값으로 설정할 수 있다.
+    function printMessage(message: string = '기본값') {
+        console.log(message);
     }
+    printMessage();
+
+    //Rest parameter
+   
+    // 전달하는 모든숫자들을 배열로 받을 수 있다.
+    function addNumbers(...numbers:number[]): number{
+        return numbers.reduce((a, b) => a + b)
+    }
+    console.log(addNumbers(1, 2));
 }
