df64aeb6983612fa527cd3fd70a1a743a0457dce
qoqomi <energyy044@gmail.com>
2022-09-02 01:06:15 +0900

    function

--- 1-types/1-1-basic.ts
+++ 1-types/1-1-basic.ts
@@ -22,6 +22,27 @@
     //unknown : 알 수 없는  어떤 데이터든 담을 수 있다. 
     let notSure: unknown = 0;
     //any
-    let anything :any 
+    let anything: any = 0;
+    anything = 'hello';
 
+    //void : 아무것도 return 하지 않는다. 
+    function print():void {
+        console.log('hello')
+        return
+    }
+    let unusable: void = undefined; //💩
+    
+    //never ->핸들링할 수 없는 경우
+    function throwError(message: string): never {
+        throw new Error(message)
+        
+        
+        
+    }
+    let obj: object; //💩 : 어떤 타입인지 정확하게 명시하는 것이 좋다. 
+    function acceptSomeObject(obj: object) {
+        acceptSomeObject({ name: 'ellie' })
+        acceptSomeObject({ animal: 'dog' })
+        
+    }
 }
---
+++ 1-types/1-2-function.ts
@@ -0,0 +1,28 @@
+{
+    //javaSctipt 💩
+    function jsAdd(num1, num2) {
+        return num1 + num2;
+    }
+    //TypeScript
+    function add(num1:number, num2:number):number {
+        return num1 + num2;
+    }
+    
+    function jsFetchNum(id) {
+        //code...
+        //code..
+        //code..
+        return new Promise((resolve,reject) => {
+            resolve(100)
+        })
+    }
+    //TypeScript
+    function jsTypeFetchNum(id:string):Promise<number> {
+        //code...
+        //code..
+        //code..
+        return new Promise((resolve,reject) => {
+            resolve(100)
+        })
+    }
+}
